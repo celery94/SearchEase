afff68f6d3db470d3407e7672b7290baccc6d76f
Celery Liu <liuct94@gmail.com>
2024-11-25 12:55:07 +0800

    refactor: remove Swagger configuration and adjust publish profile settings

--- SearchEase.Server/Program.cs
+++ SearchEase.Server/Program.cs
@@ -9,16 +9,9 @@ builder.Services.AddHostedService<LuceneIndexingService>();
 builder.Services.AddScoped<SearchService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
